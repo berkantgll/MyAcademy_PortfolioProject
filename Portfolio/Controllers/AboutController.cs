6b218bbcc1cdd8a6897af0db23d30d8e97bd9b99
berkantgll <berkantgll13@gmail.com>
2026-06-29 23:25:58 +0300

    #Added

--- Portfolio/Controllers/AboutController.cs
+++ Portfolio/Controllers/AboutController.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portfolio.Data.Context;
+using Portfolio.Data.Entities;
 
 namespace Portfolio.Controllers
 {
     public class AboutController : Controller
     {
-
         private readonly AppDbContext _context;
 
         public AboutController(AppDbContext context)
@@ -18,5 +18,20 @@ namespace Portfolio.Controllers
             var about = _context.Abouts.FirstOrDefault();
             return View(about);
         }
+
+        [HttpGet]
+        public IActionResult CreateAbout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateAbout(About about)
+        {
+            _context.Abouts.Add(about);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
